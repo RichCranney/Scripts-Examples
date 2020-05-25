be36b5e8d94b259567381b7702a14264966da8fd
RichCranney <richcranney@googlemail.com>
2020-05-25 22:01:23 +0100

    Added check for comments in file list
    If # is found at the start of the whole row, we ignore that row as we assume it is a comment.

--- PowerShell/fileShare/fileMover.ps1
+++ PowerShell/fileShare/fileMover.ps1
@@ -12,31 +12,34 @@ Move-Item $fileMoverList "$fileMoverList.processing"
 
 # Open the file and read line by line and assign to a variable
 ForEach($line in Get-Content "$fileMoverList.processing") {
-# Split the line into columns
-  $filePath, $fileName, $outputPath, $lastChecked = $line.split("|")
-# Check if the $lastChecked is null, if it is, we assume it is a new file to check and hasn't been checked before, so add a date in over 2 years ago
-  if ([string]::IsNullOrEmpty($lastChecked)) {
-    $lastChecked = (Get-Date).AddDays(-730)
-  }
-# Add this row back to the original file with current time for lastChecked
-  $currentTimestamp = Get-Date
-  Add-Content -Path $fileMoverList -Value $filePath'|'$fileName'|'$outputPath'|'$currentTimestamp
-  Write-LogEntry -logName $fileMoverLogName -LogEntry "CHECK - $fileName, checking for files since $lastChecked"
-# Now lets search the Inbound folder location for our files, which includes wildcard and last modified date!
-  Get-ChildItem $filePath -Filter $fileName -Recurse | ? {$_.LastWriteTime -gt $lastChecked} |
-  ForEach-Object {
-# For each file that is found, make sure we aren't already processing it and if we aren't send it to mainMove process. If we are, ignore it.
-    Write-LogEntry -logName $fileMoverLogName -LogEntry "FOUND - $($_.Name) found"
-    if (!(Test-Path "$processingFolder\$($_.Name)")) {
-      New-Item -Type F "$processingFolder\$($_.Name)" -force
-      $mainMoveLogDate="$(Get-Date -Format 'yyyyMMdd')"
-      $mainMoveLogName=".\logs\mainMove_$mainMoveLogDate.log"
-      Write-LogEntry -logName $fileMoverLogName -LogEntry "FOUND - $($_.Name) sent for processing - further logs can be found in $mainMoveLogName"
-      Start-Process -FilePath "powershell" -Argument ".\mainMove.ps1 -fileName '$($_.FullName)' -outputPath '$outputPath' -logName '$mainMoveLogName' -sleepSecs $sleepSecs -successCount $successCount"
-    #-WindowStyle hidden 
-    } else {
-      Write-LogEntry -logName $fileMoverLogName -LogEntry "$($_.Name) already processing - ignoring file"
-      Write-Output "$fileName already processing - alert to double check what has happened?"
+# Check the first character of the line, if it starts #, ignore it
+  if (!($line.StartsWith("#"))) {
+  # Split the line into columns
+    $filePath, $fileName, $outputPath, $lastChecked = $line.split("|")
+  # Check if the $lastChecked is null, if it is, we assume it is a new file to check and hasn't been checked before, so add a date in over 2 years ago
+    if ([string]::IsNullOrEmpty($lastChecked)) {
+      $lastChecked = (Get-Date).AddDays(-730)
+    }
+  # Add this row back to the original file with current time for lastChecked
+    $currentTimestamp = Get-Date
+    Add-Content -Path $fileMoverList -Value $filePath'|'$fileName'|'$outputPath'|'$currentTimestamp
+    Write-LogEntry -logName $fileMoverLogName -LogEntry "CHECK - $fileName, checking for files since $lastChecked"
+  # Now lets search the Inbound folder location for our files, which includes wildcard and last modified date!
+    Get-ChildItem $filePath -Filter $fileName -Recurse | ? {$_.LastWriteTime -gt $lastChecked} |
+    ForEach-Object {
+  # For each file that is found, make sure we aren't already processing it and if we aren't send it to mainMove process. If we are, ignore it.
+      Write-LogEntry -logName $fileMoverLogName -LogEntry "FOUND - $($_.Name) found"
+      if (!(Test-Path "$processingFolder\$($_.Name)")) {
+        New-Item -Type F "$processingFolder\$($_.Name)" -force
+        $mainMoveLogDate="$(Get-Date -Format 'yyyyMMdd')"
+        $mainMoveLogName=".\logs\mainMove_$mainMoveLogDate.log"
+        Write-LogEntry -logName $fileMoverLogName -LogEntry "FOUND - $($_.Name) sent for processing - further logs can be found in $mainMoveLogName"
+        Start-Process -FilePath "powershell" -Argument ".\mainMove.ps1 -fileName '$($_.FullName)' -outputPath '$outputPath' -logName '$mainMoveLogName' -sleepSecs $sleepSecs -successCount $successCount"
+      #-WindowStyle hidden 
+      } else {
+        Write-LogEntry -logName $fileMoverLogName -LogEntry "$($_.Name) already processing - ignoring file"
+        Write-Output "$fileName already processing - alert to double check what has happened?"
+      }
     }
   }
 }
